35a41b3a778e185eaa5b5b47e109e5e30258b497
Eric Taylor <eric@daxos.com>
2019-04-26 14:53:50 -0600

    feat: add @babel/types dep and switch babel-types usage

--- idx.macro.js
+++ idx.macro.js
@@ -1,4 +1,4 @@
-const t = require('babel-types');
+const t = require('@babel/types');
 const { createMacro } = require('babel-plugin-macros');
 
 function checkIdxArguments(file, node) {
@@ -6,34 +6,34 @@ function checkIdxArguments(file, node) {
   if (args.length !== 2) {
     throw file.buildCodeFrameError(
       node,
-      'The `idx` function takes exactly two arguments.',
+      'The `idx` function takes exactly two arguments.'
     );
   }
   const arrowFunction = args[1];
   if (!t.isArrowFunctionExpression(arrowFunction)) {
     throw file.buildCodeFrameError(
       arrowFunction,
-      'The second argument supplied to `idx` must be an arrow function.',
+      'The second argument supplied to `idx` must be an arrow function.'
     );
   }
   if (!t.isExpression(arrowFunction.body)) {
     throw file.buildCodeFrameError(
       arrowFunction.body,
       'The body of the arrow function supplied to `idx` must be a single ' +
-      'expression (without curly braces).',
+        'expression (without curly braces).'
     );
   }
   if (arrowFunction.params.length !== 1) {
     throw file.buildCodeFrameError(
       arrowFunction.params[2] || arrowFunction,
-      'The arrow function supplied to `idx` must take exactly one parameter.',
+      'The arrow function supplied to `idx` must take exactly one parameter.'
     );
   }
   const input = arrowFunction.params[0];
   if (!t.isIdentifier(input)) {
     throw file.buildCodeFrameError(
       arrowFunction.params[0],
-      'The parameter supplied to `idx` must be an identifier.',
+      'The parameter supplied to `idx` must be an identifier.'
     );
   }
 }
@@ -44,10 +44,10 @@ function makeCondition(node, state, inside) {
       t.BinaryExpression(
         '!=',
         t.AssignmentExpression('=', state.temp, node),
-        t.NullLiteral(),
+        t.NullLiteral()
       ),
       inside,
-      state.temp,
+      state.temp
     );
   } else {
     return node;
@@ -59,11 +59,7 @@ function makeChain(node, state, inside) {
     return makeChain(
       node.callee,
       state,
-      makeCondition(
-        t.CallExpression(state.temp, node.arguments),
-        state,
-        inside,
-      ),
+      makeCondition(t.CallExpression(state.temp, node.arguments), state, inside)
     );
   } else if (t.isMemberExpression(node)) {
     return makeChain(
@@ -72,22 +68,22 @@ function makeChain(node, state, inside) {
       makeCondition(
         t.MemberExpression(state.temp, node.property, node.computed),
         state,
-        inside,
-      ),
+        inside
+      )
     );
   } else if (t.isIdentifier(node)) {
     if (node.name !== state.base.name) {
       throw state.file.buildCodeFrameError(
         node,
         'The parameter of the arrow function supplied to `idx` must match ' +
-        'the base of the body expression.',
+          'the base of the body expression.'
       );
     }
     return makeCondition(state.input, state, inside);
   } else {
     throw state.file.buildCodeFrameError(
       node,
-      'The `idx` body can only be composed of properties and methods.',
+      'The `idx` body can only be composed of properties and methods.'
     );
   }
 }
@@ -97,26 +93,24 @@ const idx_transform = (path, state) => {
   const node = path.node;
   checkIdxArguments(state.file, node);
   const temp = path.scope.generateUidIdentifier('ref');
-  const replacement = makeChain(
-    node.arguments[1].body,
-    {
-      file: state.file,
-      input: node.arguments[0],
-      base: node.arguments[1].params[0],
-      temp,
-    },
-  );
+  const replacement = makeChain(node.arguments[1].body, {
+    file: state.file,
+    input: node.arguments[0],
+    base: node.arguments[1].params[0],
+    temp,
+  });
   path.replaceWith(replacement);
-  path.scope.push({id: temp});
-}
+  path.scope.push({ id: temp });
+};
 
 module.exports = createMacro(({ state, references }) => {
   references.default.forEach(referencePath => {
     if (referencePath.parentPath.type === 'CallExpression') {
       idx_transform(referencePath.parentPath, state);
+    } else {
+      throw Error(
+        `idx.macro can only be used a function, and can not be passed around as an argument.`
+      );
     }
-    else {
-      throw Error(`idx.macro can only be used a function, and can not be passed around as an argument.`);
-    }
-  })
+  });
 });
